615a419931ed9482e95ee64853c65b844b20b2e9
Lisa Herzberg <lisaherzberg@Lisas-MacBook-Pro.local>
2019-06-29 10:02:33 -0700

    sign up path

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -15,53 +15,63 @@ export const SignUp = (props: Props) => {
     return (
         <View style={styles.bodyContainer}>
             <Header text="Sign Up" styles={styles.headerWrapper} />
-            <TextInput
-                style={styles.input}
-                value={userName}
-                onChangeText={(input: any) => setUserName(input)}
-                placeholder="name"
-            />
-            <TextInput
-                style={styles.input}
-                value={password}
-                onChangeText={(input: any) => setPassword(input)}
-                placeholder="email"
-            />
-            <TextInput
-                style={styles.input}
-                value={password}
-                onChangeText={(input: any) => setPassword(input)}
-                placeholder="password"
-            />
-            <TextInput
-                style={styles.input}
-                value={password}
-                onChangeText={(input: any) => setPassword(input)}
-                placeholder="confirm password"
-            />
-            <CTA
-                background={Colors.green}
-                text="continue"
-                onClick={() => props.navigation.navigate('Metrics')}
-            />
+            <View style={styles.inputWrapper}>
+                <TextInput
+                    style={styles.input}
+                    value={userName}
+                    onChangeText={(input: any) => setUserName(input)}
+                    placeholder="name"
+                />
+                <TextInput
+                    style={styles.input}
+                    value={password}
+                    onChangeText={(input: any) => setPassword(input)}
+                    placeholder="email"
+                />
+                <TextInput
+                    style={styles.input}
+                    value={password}
+                    onChangeText={(input: any) => setPassword(input)}
+                    placeholder="password"
+                />
+                <TextInput
+                    style={styles.input}
+                    value={password}
+                    onChangeText={(input: any) => setPassword(input)}
+                    placeholder="confirm password"
+                />
+                <View style={styles.ctaWrapper}>
+                    <CTA
+                        background={Colors.green}
+                        text="continue"
+                        onClick={() => props.navigation.navigate('Metrics')}
+                    />
+                </View>
+            </View>
         </View>
     );
 };
 const styles = StyleSheet.create({
     bodyContainer: {
         flex: 1,
-        alignItems: 'center',
         justifyContent: 'center',
     },
     headerWrapper: {
-        paddingBottom: 20,
+        paddingHorizontal: 40,
+    },
+    inputWrapper: {
+        alignItems: 'center',
+    },
+    ctaWrapper: {
+        paddingTop: 50,
     },
     input: {
-        height: 40,
-        width: width / 1.4,
+        height: 45,
+        width: width / 1.2,
         borderWidth: 1,
         borderColor: Colors.placeholderText,
         marginVertical: 10,
-        paddingHorizontal: 10,
+        paddingHorizontal: 20,
+        borderRadius: 30,
     },
 });
